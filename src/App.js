2dd054c424677f2e944d137c9753db0a7d703a85
Sanzhar Maitanov <86508061+wa6ington@users.noreply.github.com>
2025-02-26 08:28:07 +0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/header/header';
+import RandomPlanet from './components/random-planet/random-planet';
+import ItemList from './components/item-list/item-list';
+import PersonDetails from './components/person-details/person-details';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Header />
+        <RandomPlanet />
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails />
+          </div>
+        </div>
+      </div>
   );
-}
+
+};
 
 export default App;
